62fc2b45f654f59b9a6422079320f4f72a2b5b06
Dev-LucasBarbosa <dev.lucasbarbo@gmail.com>
2024-08-18 12:48:02 -0300

    Criação da View dos produtos para o usuário

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Product
 
 # Create your views here.
+
+# Class Based View
+class ProductListView(ListView):
+    # Traz todos os produtos do BD sem filtrar nada
+    queryset = Product.objects.all()
+    template_name = "products/list.html"
+
+    #def get_context_data(self, *args, **kwargs):
+    #    context = super(ProductListView,self).get_context_data(*args, **kwargs)
+    #    print(context)
+    #    return context
