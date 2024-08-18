9cb5e1d21682c6b4a77b0e547f465c15626ea25f
Dev-LucasBarbosa <dev.lucasbarbo@gmail.com>
2024-08-18 15:25:57 -0300

    Busca por ID

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Product
@@ -16,10 +17,17 @@ class ProductListView(ListView):
     #    return context
 
 class ProductDetailView(DetailView):
-    queryset = Product.objects.all()
+    #queryset = Product.objects.all()
     template_name = "products/detail.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView,self).get_context_data(*args, **kwargs)
         print(context)
         return context
+    
+    def get_object(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Esse produto não existe!")
+        return instance
