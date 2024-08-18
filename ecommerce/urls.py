3ee735289348bfa686591cfd7c2ec0cf93faa85d
Dev-LucasBarbosa <dev.lucasbarbo@gmail.com>
2024-08-18 13:18:44 -0300

    Criação dos detalhes de cada produto

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,7 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from products.views import ProductListView
+from products.views import ProductListView, ProductDetailView
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -28,6 +28,7 @@ urlpatterns = [
     path('login/', login_page),
     path('register', register_page),
     path('products/', ProductListView.as_view()),
+    path('products/<int:pk>', ProductDetailView.as_view()),
     path('admin/', admin.site.urls),
 ]
 
