a92297d4a52faf577cd6cb684bb50a2488e99cf1
Dev-LucasBarbosa <dev.lucasbarbo@gmail.com>
2024-08-18 19:40:34 -0300

    Criação dos itens em destaque

--- products/views.py
+++ products/views.py
@@ -1,9 +1,25 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.http import Http404
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Product
 
 # Create your views here.
+
+class ProductFeaturedListView(ListView):
+    template_name = "products/list.html"
+
+    def get_queryset(self, *args, **kwargs):
+        return Product.objects.featured()
+    
+class ProductFeaturedDetailView(DetailView):
+    queryset = Product.objects.all().featured()
+    template_name = "products/featured-detail.html"
+
+    #def get_queryset(self, *args, **kwargs):
+        #request = self.request
+        #return Product.objects.featured()
 
 # Class Based View
 class ProductListView(ListView):
