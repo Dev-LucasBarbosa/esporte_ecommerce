bc6cdf367caa78c36b1aaf3c047d8998d4aceae4
Dev-LucasBarbosa <dev.lucasbarbo@gmail.com>
2024-08-19 11:31:59 -0300

    Criação do slug

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from typing import Any
+from django.db.models.base import Model as Model
 from django.db.models.query import QuerySet
 from django.http import Http404
 from django.shortcuts import render
@@ -46,4 +47,19 @@ class ProductDetailView(DetailView):
         instance = Product.objects.get_by_id(pk)
         if instance is None:
             raise Http404("Esse produto não existe!")
+        return instance
+    
+class ProductDetailSlugView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_object(self, *args, **kwargs):
+        slug = self.kwargs.get('slug')
+        try:
+            instance = Product.objects.get(slug = slug, active = True)
+        except Product.DoesNotExist:
+            raise Http404("Não encontrado!")
+        except Product.MultipleObjectsReturned:
+            qs = Product.objects.filter(slug = slug, active = True)
+            instance =  qs.first()
         return instance
