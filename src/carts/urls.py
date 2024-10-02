e66c81e69bd6b360414ad5ed8a5767098db75c13
Lucas Antonio <dev.lucasbarbo@gmail.com>
2024-10-02 14:21:15 -0300

    Mexendo um pouco com JQuery

--- src/carts/urls.py
+++ src/carts/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 
 app_name = "carts"
 
-from .views import cart_page, checkout_home, cart_update
+from .views import cart_page, checkout_home, cart_update, checkout_done_view
 
 urlpatterns = [
     path('', cart_page, name='home'),
+    path('checkout/success/', checkout_done_view, name='success'),
     path('checkout/', checkout_home, name='checkout'),
     path('update/', cart_update, name='update')
 ]
