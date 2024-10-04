6fa3896be31641a72c5d7c642201f805b3f5351d
Lucas Antonio <dev.lucasbarbo@gmail.com>
2024-10-04 13:03:29 -0300

    Implementação do gerenciador do modelo User

--- src/ecommerce/settings.py
+++ src/ecommerce/settings.py
@@ -53,6 +53,8 @@ INSTALLED_APPS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'accounts.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
