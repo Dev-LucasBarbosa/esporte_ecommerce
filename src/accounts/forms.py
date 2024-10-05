4b7be0de197526e23b1e2380bfa24d278e61d24e
Dev-LucasBarbosa <dev.lucasbarbo@gmail.com>
2024-10-05 12:58:02 -0300

    Atualizando os formulários de registro e login

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -43,33 +43,30 @@ class GuestForm(forms.Form):
     email = forms.EmailField()
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
+    username = forms.EmailField(label='Email')
     password = forms.CharField(widget=forms.PasswordInput)
 
-class RegisterForm(forms.Form):
-    username = forms.CharField()
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+class RegisterForm(forms.ModelForm):
 
-    def clean_username(self):
-        username = self.cleaned_data.get('username')
-        qs = User.objects.filter(username=username)
-        if qs.exists():
-            raise forms.ValidationError("Esse usuário já existe, escolha outro nome.")
-        return username
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_2 = forms.CharField(label='Confirme a Senha', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['email']
     
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        qs = User.objects.filter(email=email)
-        if qs.exists():
-            raise forms.ValidationError("Esse email já existe, tente outro!")
-        return email
-
     def clean(self):
-        data = self.cleaned_data
-        password = self.cleaned_data.get('password')
-        password2 = self.cleaned_data.get('password2')
-        if password != password2:
-            raise forms.ValidationError("As senhas informadas devem ser iguais!")
-        return data
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_2 = cleaned_data.get("password_2")
+        if password is not None and password != password_2:
+            self.add_error("password_2", "Suas senhas devem ser iguais")
+        return cleaned_data
+    
+    def save(self, commit=True):
+        user = super(RegisterForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        user.active = False
+        if commit:
+            user.save()
+        return user
